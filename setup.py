4ae98c163d26239468b71493f77b65cb7cbf4b00
Elijah Smith <esmith1412@posteo.net>
2020-03-27 12:39:10 -0400

    Added setup.py to project

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convert-rhythmbox-to-sandisk-gui',
+    version='1.0.0.dev1',
+    description='A GUI application that takes an M3U playlist exported from Rhythmbox, and converts it to a format that can be read by the SanDisk Sansa music player, running Rockbox firmware',
+    url='https://github.com/esmith1412/convert-rhythmbox-to-sandisk-gui',
+    author='Elijah Smith',
+    license='GNU GPLv3',
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+    ],
+    python_requires='~=3.7.3'
+)
